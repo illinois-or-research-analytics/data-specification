71696928c9c06096e19e4f8f6db452a1d87fa14e
Haotian Yi <yi54@illinois.edu>
2025-10-17 00:05:02 -0500

    sanity: put an example edgelist file

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 mkdir -p test-script-out
 rm test-script-out/*
 
-test_inputs=("inputs/network.csv")
+test_inputs=("examples/edgelist.csv")
 pipelines=("ikc+aoc" "infomap+cc" "infomap+wcc" "leiden+cc" "leiden+cm" "sbm")
 for test_input in "${test_inputs[@]}"; do
     for pipeline in "${pipelines[@]}"; do
         echo "Running examples/$pipeline.json"
-        python pipeline.py examples/$pipeline.json inputs/network.csv -o test-script-out/result-$pipeline.csv
+        python pipeline.py examples/$pipeline.json $test_inputs -o test-script-out/result-$pipeline.csv
 
         if [ ! -f test-script-out/result-$pipeline.csv ]; then
             echo "Error: $pipeline did not produce a valid output at test-script-out/result-$pipeline.csv"
