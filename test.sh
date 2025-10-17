637b8046ccab3825fc3ee981b91ce86bc7f7603c
Haotian Yi <yi54@illinois.edu>
2025-10-16 20:46:02 -0500

    test: testing script

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p test-script-out
+rm test-script-out/*
+
+test_inputs=("inputs/network.csv")
+pipelines=("ikc+aoc" "infomap+cc" "infomap+wcc" "leiden+cc" "leiden+cm" "sbm")
+for test_input in "${test_inputs[@]}"; do
+    for pipeline in "${pipelines[@]}"; do
+        echo "Running examples/$pipeline.json"
+        python pipeline.py examples/$pipeline.json inputs/network.csv -o test-script-out/result-$pipeline.csv
+
+        if [ ! -f test-script-out/result-$pipeline.csv ]; then
+            echo "Error: $pipeline did not produce a valid output at test-script-out/result-$pipeline.csv"
+            exit 1
+        fi
+    done
+done
+
+echo "All tests passed!"
